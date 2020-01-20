bd86b675974abfc87ed018c0ee8d011238487622
gihop <giho-p@hanmail.net>
2020-01-20 17:53:16 +0900

    자와_각도기_2916 풀이보고 다시 품, 다시 볼 필요 있음

--- 브루트포스/브루트포스/자와_각도기_2916.cpp
+++ 브루트포스/브루트포스/자와_각도기_2916.cpp
@@ -7,53 +7,37 @@
 //
 #include <iostream>
 
-
 using namespace std;
 
-int angle[10], angleCount[10];
-int n;
+//기존에 내가 풀었던 방법과 달리 알고 있는 각으로 만들 수 있는 모든 각을 미리 만들어 놓고
+//입력된 값이 배열에서 true인지 false인지만 판단해서 출력.
 
-int cal(int result, int result2, int sum, int idx){
-    cout << sum << endl;
-    if(abs(sum) == result)
-        return 1;
-    if(abs(sum) == result2)
-        return 1;
-    if(sum > 360 || sum < 0)
-        return 0;
-    if(angleCount[idx] > 720){
-        return 0;
-    }
-    for(int i=0; i<n; i++){
-        sum += angle[i];
-        angleCount[i] += angle[i];
-        if(cal(result, result2, sum, i))
-            return 1;
-        sum -= angle[i] * 2;
-        angleCount[i] += angle[i];
-        if(cal(result, result2, sum, i))
-            return 1;
-        sum += angle[i];
-    }
-    return 0;
-}
+int a[10];
+bool d[360];
 
 int main(){
-    int k, result[10];
+    int n, m;
     
-    cin >> n >> k;
+    cin >> n >> m;
     
     for(int i=0; i<n; i++)
-        cin >> angle[i];
+        cin >> a[i];
     
-    for(int i=0; i<k; i++)
-        cin >> result[i];
+    d[0] = true;//반복문이 처음 실행될 때 반복적으로 더하기 위함.
+    for(int i=0; i<n; i++){//a배열 숫자 하나로 만들 수 있는 모든 숫자 조합 계산
+        for(int j=0; j<360; j++){//다른 추가된 숫자 조합을 계속 갱신, 처음 숫자가 1이여도 359까지 가기 위함
+            for(int k=0; k<360; k++){
+                if(d[k] == false) continue;//나왔었던 숫자인 경우만 계산하기 위함.
+                d[(k-a[i]+360)%360] = true;
+                d[(k+a[i])%360] = true;
+            }
+        }
+    }
     
-    for(int i=0; i<k; i++){
-        if(cal(result[i], 360-result[i], 0, 0))
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+    for(int i=0; i<m; i++){
+        int x;
+        cin >> x;
+        cout << (d[x] ? "YES" : "NO") << endl;
     }
     
     return 0;
