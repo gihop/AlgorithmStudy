42f50da34397ebe3b64c49a28eadfbe5d7a978f9
gihop <giho-p@hanmail.net>
2020-01-20 21:23:35 +0900

    주석 추가

--- 브루트포스/브루트포스/부분수열의_합_14225.cpp
+++ 브루트포스/브루트포스/부분수열의_합_14225.cpp
@@ -10,10 +10,14 @@
 
 using namespace std;
 
+//한윤정이_이탈리아에_가서_2422 문제의 풀이방법을 차용함.
+//크기가 큰 bool 타입 배열을 만들어 놓고 가능한 경우의 수를 인덱스로 하여 해당 원소를 true로 바꿈.
+//1부터 시작해서 false가 나오는 곳을 정답을 출력.
+
 bool a[10000000];
 int n, s[20];
 
-void cal(int num, int idx){
+void cal(int num, int idx){//부분수열에서 가능한 모든 경우의 수를 구함.
     for(int i=idx+1; i<n; i++){
         num += s[i];
         a[num] = true;
