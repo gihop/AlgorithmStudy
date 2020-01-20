59e0783097e20fb0d9d4d9a7c6eeaf717c74ff61
gihop <giho-p@hanmail.net>
2020-01-20 19:09:23 +0900

    한윤정이_이탈리아에_가서_2422 문제 해결, 피해야 하는 조합을 합쳐서 일차원 배열에 저장하는 것이 아니라 이차원 배열에 인덱스에 맞게 저장해서 바로 접근하여 검사하게 함

--- 브루트포스/브루트포스/한윤정이_이탈리아에_가서_2422.cpp
+++ 브루트포스/브루트포스/한윤정이_이탈리아에_가서_2422.cpp
@@ -11,22 +11,20 @@
 
 using namespace std;
 
-set<int> s;
-int a[10000];
+//이전 풀이의 문제점 : set의 사용 이유가 없음, 피해야 하는 아이스크림 종류를 항상 반복문으로 탐색.
+//바꾼점 : 피해야 하는 아이스크림 종류를 이차원배열에 저장함으로써 n의 인자로 바로 검사할 수 있음.
+
+bool a[10000][10000];//피해야 하는 조합을 숫자순으로 저장.
 int n, m, ans;
 
-void pick(int num, int cnt, int idx){
-    if(cnt == 3){
-        for(int i=0; i<m; i++)
-            if(a[i] == num/10 || a[i] == num%100)
-                return;
-        s.insert(num);
-        return;
-    }
-    for(int i=idx+1; i<=n; i++){
-        num = (num*10) + i;
-        pick(num, cnt+1, i);
-        num/=10;
+void pick(){//아이스크림 모든 조합에서 피해야 하는 종류 검사함.
+    for(int i=1; i<=n-2; i++){
+        for(int j=i+1; j<=n-1; j++){
+            for(int k=j+1; k<=n; k++){
+                if(a[i][j] || a[i][k] || a[j][k]) continue;//피해야 하는 조합일 경우.
+                ans++;
+            }
+        }
     }
 }
 
@@ -34,18 +32,17 @@ int main(){
     cin >> n >> m;
     
     for(int i=0; i<m; i++){
-        int x, y, z;
+        int x, y;
         cin >> x >> y;
         if(x > y)
-            z = (10*y)+x;
+            a[y][x] = true;
         else
-            z = (10*x)+y;
-        a[i] = z;
+            a[x][y] = true;
     }
     
-    pick(0, 0, 0);
+    pick();
     
-    cout << s.size();
+    cout << ans;
     
     return 0;
 }
