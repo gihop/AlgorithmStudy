8dcb6f723d07a0a65fd07a28fcb9d961fbabd14f
gihop <giho-p@hanmail.net>
2020-02-15 16:26:42 +0900

    열쇠_9328 문제 해결. 문제를 풀 수 있었던 아이디어가 중요하다. 구현이 꽤 어려웠다. 주석 참조.

--- BFS/BFS/열쇠_9328.cpp
+++ BFS/BFS/열쇠_9328.cpp
@@ -18,6 +18,11 @@
 #define endl "\n"
 using namespace std;
 
+//이 문제를 풀 수 있었던 핵심 아이디어는 문을 만났는데 문을 열 수 있는 키가 없다면 문의 좌표를 저장해놓고
+//나중에 키를 발견했는데 저장했던 문의 키라면 문의 좌표를 큐에 넣는다. 즉, 문을 들어간다.
+//틀렸습니다가 나왔던 이유는 건물안으로 들어갈 수 있는 테두리가 빈칸 뿐만이 아닌 문과 키, 문서도 가능한 것을 간과했다.
+//구현이 굉장히 복잡하다.
+
 int tc;
 int n,m;
 char a[100][100];
@@ -36,25 +41,50 @@ int main(){
         cin >> n >> m;
         queue<pair<int,int>> q;
         memset(check, false, sizeof(check));
-        for(int j=0; j<n; j++){
+        int ans=0;
+        set<char> s;//이 세트에는 발견한 키들을 모두 저장한다. 문을 만났을 때 갖고 있는 키인지 빠르게 찾는 역할이다.
+        map<char,int> Map;//이 맵은 키를 발견했을 때 이전에 만났었던 문이 있는지 빠르게 찾아주는 역할을 한다. 즉 이 맵에 'a'라는 키의 count가 1이면 밑에 줄 벡터에 그 문의 좌표가 저장되어있다는 뜻이다.
+        vector<vector<pair<int,int>>> v(26);//알파벳의 갯수 만큼 벡터를 만들어준다. 이 벡터에는 열쇠가 없는 문을 만났을 경우 문의 좌표를 이 벡터에 저장한다.
+        for(int j=0; j<n; j++){//맵의 정보를 입력하고 맵의 테두리에 벽이 아닌 부분(빈공간, 문서, 키, 문)을 검사한다. 즉 모든 입구를 큐에 넣는다.
             for(int k=0; k<m; k++){
                 cin >> a[j][k];
-                if(((j==0 || j==n-1) || (k==0 || k==m-1)) && a[j][k]=='.'){
+                if(((j==0 || j==n-1) || (k==0 || k==m-1)) && (a[j][k]=='.')){
                     q.emplace(j,k);
                     check[j][k]=true;
+                }
+                else if(((j==0 || j==n-1) || (k==0 || k==m-1)) && (a[j][k]=='$')){
+                    q.emplace(j,k);
+                    check[j][k]=true;
+                    ans++;//입구가 문서일 경우에는 ans+1.
+                }
+                else if(((j==0 || j==n-1) || (k==0 || k==m-1)) && (a[j][k]>='a' && a[j][k]<='z')){
+                    q.emplace(j,k);
+                    check[j][k]=true;
+                    s.insert(a[j][k]);//입구가 키인 경우에는 set에 추가한다.
+                }
+                else if(((j==0 || j==n-1) || (k==0 || k==m-1)) && (a[j][k]>='A' && a[j][k]<='Z')){
+                    Map[a[j][k]-'A'+'a']=1;
+                    v[(int)(a[j][k]-'A')].push_back(make_pair(j,k));//입구가 문인 경우에는 바로 큐에 넣지 않고 키를 발견했을 경우 큐에 넣는다.
                 }
             }
         }
         string key;
-        cin >> key;
-        set<char> s;
-        if(key!="0"){
-            for(int j=0; j<key.size(); j++)
+        cin >> key;//빌딩 바닥에 놓여져 있는 키.
+        if(key!="0"){//키가 0이 아닌 경우.
+            for(int j=0; j<key.size(); j++){
                 s.insert(key[j]);
+                if(Map.count(key[j])>0){//이전에 빌딩의 테두리를 검사할 때 있었던 문의 키인 경우.
+                    for(int k=0; k<v[(int)(key[j]-'a')].size(); k++){
+                        int x, y;
+                        tie(x,y) = v[(int)(key[j]-'a')][k];
+                        q.emplace(x,y);
+                        check[x][y]=true;
+                    }
+                }
+                Map.erase(key[j]);//맵과 벡터에 저장된 문을 키로 열었다면 지워준다.
+                v[(int)(key[j])-'a'].clear();
+            }
         }
-        map<char,int> Map;
-        int ans=0;
-        vector<vector<pair<int,int>>> v(26);
         while(!q.empty()){
             int x,y;
             tie(x,y) = q.front();
@@ -65,35 +95,35 @@ int main(){
                 ny = y+dy[j];
                 if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
                 if(check[nx][ny]) continue;
-                if(a[nx][ny]=='*') continue;
-                else if(a[nx][ny]>='a' && a[nx][ny]<='z'){
-                    if(Map.count(a[nx][ny])>0){
-//                        cout << (int)(a[nx][ny]-'a');
+                if(a[nx][ny]=='*') continue;//벽인 경우.
+                else if(a[nx][ny]>='a' && a[nx][ny]<='z'){//키인 경우.
+                    if(Map.count(a[nx][ny])>0){//이 키의 문을 만난적이 있는 경우.
                         for(int k=0; k<v[(int)(a[nx][ny]-'a')].size(); k++){
                             int tx, ty;
-                            
                             tie(tx,ty)=v[(int)(a[nx][ny]-'a')][k];
-                            q.emplace(tx,ty);
+                            q.emplace(tx,ty);//해당 문을 연다.
                             check[tx][ty]=true;
                         }
+                        Map.erase(a[nx][ny]);//저장된 문의 정보는 삭제한다.
+                        v[(int)(a[nx][ny]-'a')].clear();
                     }
-                    s.insert(a[nx][ny]);
+                    s.insert(a[nx][ny]);//키 set에 삽입.
                     q.emplace(nx,ny);
                     check[nx][ny]=true;
                 }
-                else if(a[nx][ny]>='A' && a[nx][ny]<='Z'){
-                    if(s.count(a[nx][ny]-'A'+'a')>0){
+                else if(a[nx][ny]>='A' && a[nx][ny]<='Z'){//문인 경우.
+                    if(s.count(a[nx][ny]-'A'+'a')>0){//해당하는 문의 키가 있는지 확인한다.
                         q.emplace(nx,ny);
                         check[nx][ny]=true;
-                    }else{
+                    }else{//키가 없는 경우 맵과 벡터에 저장한다.
                         Map[a[nx][ny]-'A'+'a']=1;
                         v[(int)(a[nx][ny]-'A')].push_back(make_pair(nx,ny));
                     }
-                }else if(a[nx][ny]=='$'){
+                }else if(a[nx][ny]=='$'){//문서를 발견한 경우.
                     q.emplace(nx,ny);
                     check[nx][ny]=true;
                     ans++;
-                }else if(a[nx][ny]=='.'){
+                }else if(a[nx][ny]=='.'){//빈공간인 경우.
                     q.emplace(nx,ny);
                     check[nx][ny]=true;
                 }
