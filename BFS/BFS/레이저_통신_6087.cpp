30f9ff413b55de86c4fe1c5a79fcc4c2bcebc225
gihop <giho-p@hanmail.net>
2020-02-19 15:18:00 +0900

    레이저_통신_6087 주석 추가.

--- BFS/BFS/레이저_통신_6087.cpp
+++ BFS/BFS/레이저_통신_6087.cpp
@@ -13,6 +13,9 @@
 #define endl "\n"
 using namespace std;
 
+//쉬워보이지만 생각을 잘해야 하는 문제.
+//큐에 방향을 같이 삽입해서 다음에 갈 방향이 이전에 움직였던 방향과 다르면 cnt+1을 한다.
+
 char a[100][100];
 int check[100][100];
 int n,m;
@@ -45,21 +48,21 @@ int main(){
         int x,y,cnt,dirct;
         tie(x,y,cnt,dirct) = q.front();
         q.pop();
-        if(a[x][y]=='C' && (x!=ax || y!=ay)){
+        if(a[x][y]=='C' && (x!=ax || y!=ay)){//다른 C에 도착했을 경우.
             if(ans > cnt)
                 ans = cnt;
             continue;
         }
         for(int i=0; i<4; i++){
             int nx, ny, nCnt;
-            if(dirct==-1 || dirct == i)
+            if(dirct==-1 || dirct == i)//-1은 처음 C에서 출발할 때를 말하고 방향이 같게 움직이면 cnt는 그대로
                 nCnt = cnt;
-            else if(dirct != i)
+            else if(dirct != i)//방향이 다르게 움직이면 cnt+1.
                 nCnt = cnt+1;
             nx = x+dx[i];
             ny = y+dy[i];
             if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-            if(check[nx][ny]!=0 && check[nx][ny]<nCnt) continue;
+            if(check[nx][ny]!=0 && check[nx][ny]<nCnt) continue;//이미 방문했던 칸인데 cnt값이 check에 저장된 값보다 큰 경우.
             if(a[nx][ny]=='*') continue;
             q.emplace(nx,ny,nCnt,i);
             check[nx][ny]=nCnt;
