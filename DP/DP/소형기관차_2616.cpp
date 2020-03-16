769d1d45282c5519a97f448377f3283342f086fb
gihop <giho-p@hanmail.net>
2020-03-16 20:19:19 +0900

    소형기관차_2616 내일 다시 도전.

---
+++ DP/DP/소형기관차_2616.cpp
@@ -0,0 +1,20 @@
+//
+//  소형기관차_2616.cpp
+//  DP
+//
+//  Created by jiho park on 2020/03/18.
+//  Copyright © 2020 jiho park. All rights reserved.
+//
+
+#include <iostream>
+
+#define endl "\n"
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    return 0;
+}
