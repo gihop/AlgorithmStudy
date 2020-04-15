74d5a5d85689db89858805233d75b1d5ff07e10c
gihop <giho-p@hanmail.net>
2020-04-15 19:34:51 +0900

    라면공장 문제 해결.

---
+++ Programmers/Programmers/라면공장.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(int stock, vector<int> dates, vector<int> supplies, int k) {
+    int answer = 0;
+    
+    priority_queue<int, vector<int>, less<int>> pq;
+    
+    for(int day=0,j=0; day<k; day++){
+        if(dates.size()>j && day>=dates[j]){
+            pq.push(supplies[j]);
+            j++;
+        }
+        if(!stock){
+            stock+=pq.top();ㅎ
+            pq.pop();
+            answer++;
+        }
+        stock--;
+    }
+    
+    return answer;
+}
