1d02291bdccc6d5bec0a9b5bf1311ae3e75a62ce
gihop <giho-p@hanmail.net>
2020-07-12 16:51:10 +0900

    내려가기_2096 문제 해결. 메모리 초과를 의외로 간단한 방법으로 해결할 수 있었다. 주석 참고.

--- DP/DP/내려가기_2096.cpp
+++ DP/DP/내려가기_2096.cpp
@@ -8,13 +8,20 @@
 
 #include <iostream>
 #include <cstring>
-#include <algorithm>
 
 using namespace std;
 
+//처음에는 dp 배열을 dp[100000][3] 크기로 해서 메모리 초과가 발생했다.
+//이 문제의 메모리 제한이 4mb로 적기 때문이다.
+//이를 해결하기 위해 dp[2][3]으로 바꿨다.
+//dp배열을 채워나갈 때 2개의 행만 필요하고 나머지를 계속 저장하고 있을 필요가 없기 때문이다.
+
 int n;
 int a[100000][3];
-int dp[100000][3];
+int dp[2][3];
+
+int bigger(int x, int y){ if(x>y) return x; return y; }
+int smaller(int x, int y){ if(x>y) return y; return x; }
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -30,42 +37,29 @@ int main(){
         dp[0][i] = a[0][i];
     
     for(int i=1; i<n; i++){
-        if(dp[i-1][0]){
-            if(dp[i][0]<dp[i-1][0]+a[i][0]) dp[i][0]=dp[i-1][0]+a[i][0];
-            if(dp[i][1]<dp[i-1][0]+a[i][1]) dp[i][1]=dp[i-1][0]+a[i][1];
-        }
-        if(dp[i-1][1]){
-            if(dp[i][0]<dp[i-1][1]+a[i][0]) dp[i][0]=dp[i-1][1]+a[i][0];
-            if(dp[i][1]<dp[i-1][1]+a[i][1]) dp[i][1]=dp[i-1][1]+a[i][1];
-            if(dp[i][2]<dp[i-1][1]+a[i][2]) dp[i][2]=dp[i-1][1]+a[i][2];
-        }
-        if(dp[i-1][2]){
-            if(dp[i][1]<dp[i-1][2]+a[i][1]) dp[i][1]=dp[i-1][2]+a[i][1];
-            if(dp[i][2]<dp[i-1][2]+a[i][2]) dp[i][2]=dp[i-1][2]+a[i][2];
+        dp[1][0] = a[i][0]+bigger(dp[0][0], dp[0][1]);
+        dp[1][1] = a[i][1]+bigger(dp[0][0], bigger(dp[0][1], dp[0][2]));
+        dp[1][2] = a[i][2]+bigger(dp[0][1], dp[0][2]);
+        for(int j=0; j<3; j++){//이 부분에서 dp 배열을 2행만 쓰기 위해서 1행을 0행으로 땡긴다.
+            dp[0][j] = dp[1][j];
+            dp[1][j]=0;
         }
     }
-    int Max;
-    if(dp[n-1][0]>dp[n-1][1]) Max=dp[n-1][0];
-    else Max = dp[n-1][1];
-    if(Max<dp[n-1][2]) Max = dp[n-1][2];
+    int Max = bigger(dp[0][0], bigger(dp[0][1],dp[0][2]));
     memset(dp, 0, sizeof(dp));
+    
     for(int i=0; i<3; i++)
         dp[0][i] = a[0][i];
     for(int i=1; i<n; i++){
-        dp[i][0]=dp[i-1][0]+a[i][0];
-        dp[i][1]=dp[i-1][1]+a[i][1];
-        dp[i][2]=dp[i-1][2]+a[i][2];
-        if(dp[i][1]>dp[i-1][0]+a[i][1]) dp[i][1]=dp[i-1][0]+a[i][1];
-        
-        if(dp[i][0]>dp[i-1][1]+a[i][0]) dp[i][0]=dp[i-1][1]+a[i][0];
-        if(dp[i][2]>dp[i-1][1]+a[i][2]) dp[i][2]=dp[i-1][1]+a[i][2];
-        
-        if(dp[i][1]>dp[i-1][2]+a[i][1]) dp[i][1]=dp[i-1][2]+a[i][1];
+        dp[1][0]=a[i][0]+smaller(dp[0][0], dp[0][1]);
+        dp[1][1]=a[i][1]+smaller(dp[0][0], smaller(dp[0][1], dp[0][2]));
+        dp[1][2]=a[i][2]+smaller(dp[0][1], dp[0][2]);
+        for(int j=0; j<3; j++){
+            dp[0][j] = dp[1][j];
+            dp[1][j]=0;
+        }
     }
-    int Min;
-    if(dp[n-1][0]>dp[n-1][1]) Min=dp[n-1][1];
-    else Min = dp[n-1][0];
-    if(Min>dp[n-1][2]) Min = dp[n-1][2];
+    int Min=smaller(dp[0][0], smaller(dp[0][1], dp[0][2]));
     
     cout << Max << " " << Min << endl;
 
