93326d8a5aa98304526e5ab19b576710d741b546
gihop <giho-p@hanmail.net>
2020-08-06 20:22:21 +0900

    기능개발_Programmers 문제 해결. Kotlin으로 알고리즘 공부 시작.

---
+++ Kotlin/src/기능개발_Programmers.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+        val answer: MutableList<Int> = arrayListOf()
+        val days = progresses
+
+        for(i in 0 until progresses.size){
+            days[i] = ((100 - progresses[i])/speeds[i])
+        }
+
+        var max = days[0]
+        var releaseCount = 1
+
+        for(i in 1 until days.size){
+            if(max < days[i]){
+                answer.add(releaseCount)
+                releaseCount = 1
+                max = days[i]
+            }
+            else{
+                releaseCount++
+            }
+        }
+
+        answer.add(releaseCount)
+
+        return answer.toIntArray()
+    }
+}
