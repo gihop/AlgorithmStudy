28b524fc1f5c92f6bec42b99b14939a7bfe46dad
gihop <giho-p@hanmail.net>
2021-03-11 23:13:06 +0900

    Two Sum 문제 해결.

---
+++ 부스트캠프/python/Two Sum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in dic:
+                dic[num] = i
+            else:
+                return [dic[n], i]
