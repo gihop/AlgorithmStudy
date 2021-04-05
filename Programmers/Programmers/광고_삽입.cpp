bb96ec39c28447bf612ccc674ea01dd196eed4a0
gihop <giho-p@hanmail.net>
2021-04-05 21:06:23 +0900

    광고_삽입 문제 해결. 생각지도 못한 간단한 방법이 존재했다. 꼭 다시 보자. 주석 참고.

--- Programmers/Programmers/광고_삽입.cpp
+++ Programmers/Programmers/광고_삽입.cpp
@@ -3,8 +3,32 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <queue>
 
 using namespace std;
+
+/*
+ 처음에는 풀지 못했던 문제.
+ 시간이 충분히 주어져도 풀지 못할 것이라고 생각했었지만 굉장히 간단한 풀이가 있었다.
+ 기존의 풀이는 solution2이고 solution으로 풀 수 있었다.
+ 최대 play_time이 99:59:59이므로 100:00:00을 초단위로 바꾼 360000초의 배열에
+ 각 log의 시작과 끝을 숫자로 변경하여 배열에 1씩 더해주고 이를 avd_time을 sliding window
+ 방식으로 탐색하여 최대 광고 재생 시작 시간을 찾는 방식이다.
+ */
+
+/*
+ 주의할 점
+ 1) 광고 재생 시간이 00:00:00~00:00:10이면 배열에 11개를 더하는 것이 아닌 00:00:00부터
+ 00:00:09인 10개를 더하는 것이다.
+ 이 부분 때문에 한 번 틀렸었다.
+ 
+ 2) 한 개의 테스트 케이스가 틀렸다고 결과가 나왔는데
+ 이는 sliding window 방식을 할 때 최대 재생 시간의 크기와 window의 재생 시간 크기를 저장하는
+ 변수가 int로는 담을 수 없었기 때문이었다.
+ 이를 long 타입 변수로 바꿔서 해결할 수 있었다.
+ */
+
+int adv[360000];
 
 int time_to_int(string time_string){
     int hour = stoi(time_string.substr(0,2));
@@ -35,6 +59,48 @@ string time_to_string(int time_int){
 }
 
 string solution(string play_time, string adv_time, vector<string> logs) {
+    string answer = "00:00:00";
+    
+    for(int i=0; i<logs.size(); i++){
+        string start = "", end = "";
+        int split = logs[i].find('-');
+        start = logs[i].substr(0, split);
+        end = logs[i].substr(split+1, logs[i].size());
+        
+        int start_time = time_to_int(start);
+        int end_time = time_to_int(end);
+        for(int j=start_time; j<end_time; j++){
+            adv[j] += 1;
+        }
+    }
+    
+    int adv_end = time_to_int(adv_time);
+    int play_end = time_to_int(play_time);
+    long max_time = 0;
+    
+    queue<int> q;
+    for(int i=0; i<adv_end; i++){
+        max_time += adv[i];
+        q.emplace(i);
+    }
+    
+    long now_time = max_time;
+    for(int i=adv_end; i<=play_end; i++){
+        int front_time = q.front();
+        q.pop();
+        q.emplace(i);
+        now_time -= adv[front_time];
+        now_time += adv[i];
+        if(now_time > max_time){
+            max_time = now_time;
+            answer = time_to_string(q.front());
+        }
+    }
+    
+    return answer;
+}
+
+string solution2(string play_time, string adv_time, vector<string> logs) {
     string answer = "";
     
     vector<pair<string,string>> log_table(logs.size(), make_pair("", ""));
@@ -96,10 +162,10 @@ string solution(string play_time, string adv_time, vector<string> logs) {
 }
 
 int main(){
-//    vector<string> logs = {"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"};
-//    cout << solution("99:59:59", "25:00:00", logs);
-    vector<string> logs = {"15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"};
-    cout << solution("50:00:00", "50:00:00", logs);
+    vector<string> logs = {"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"};
+    cout << solution2("99:59:59", "25:00:00", logs);
+//    vector<string> logs = {"15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"};
+//    cout << solution2("50:00:00", "50:00:00", logs);
     
     return 0;
 }
